2442d3bc16934d831d4093d44f459ec6e0bf80f1
Colin Clark <colinbdclark@gmail.com>
2017-11-12 18:59:27 -0500

    (in)sanity commit. Significant cleanup required in the frag shader.

--- src/js/the-long-distributed-way.js
+++ src/js/the-long-distributed-way.js
@@ -11,6 +11,10 @@ fluid.defaults("colin.theLongWay", {
             type: "colin.theLongWay.videoLayer"
         },
 
+        modulationLayer: {
+            type: "colin.theLongWay.modulationLayer"
+        },
+
         glRenderer: {
             type: "colin.theLongWay.glRenderer"
         }
@@ -18,12 +22,115 @@ fluid.defaults("colin.theLongWay", {
 });
 
 fluid.defaults("colin.theLongWay.videoLayer", {
-    gradeNames: "aconite.compositableVideo.layer",
+    gradeNames: "aconite.compositableVideo",
 
     url: "videos/moitessier-la-longue-route.mp4",
 
     model: {
         loop: true
+    }
+});
+
+fluid.defaults("colin.theLongWay.modulationLayer", {
+    gradeNames: "colin.theLongWay.videoLayer",
+
+    bindToTextureUnit: "TEXTURE1",
+
+    model: {
+        inTime: 14,
+        outTime: 27
+    },
+
+    clips: {
+        "drinking coffee": {
+            inTime: 4 * 60 + 27,
+            outTime: 5 * 60
+        },
+
+        "sails from the mast": {
+            inTime: 5 * 60 + 36,
+            outTime: 6 * 60 + 9
+        },
+
+        "first sunset": {
+            inTime: 6 * 60 + 18,
+            outTime: 6 * 60 + 29
+        },
+
+        "les oiseaux de terres": {
+            inTime: 9 * 60 + 22,
+            outTime: 9 * 60 + 52
+        },
+
+        "dolphin squares": {
+            inTime: 10 * 60 + 32,
+            outTime: 10 * 60 + 45
+        },
+
+        "dolphin squares 2": {
+            inTime: 11 * 60 + 7,
+            outTime: 11 * 60 + 44
+        },
+
+        "sails from the mast 2": {
+            inTime: 17 * 60 + 8,
+            outTime: 17 * 60 + 59
+        },
+
+        "albatrosses in calm": {
+            inTime: 18 * 60 + 20,
+            outTime: 19 * 60
+        },
+
+        "cheese-eating birds with lots of squares": {
+            inTime: 19 * 60 + 45,
+            outTime: 19 * 50 + 56
+        },
+
+        "albatrosses in swells": {
+            inTime: 19 * 60 + 57,
+            outTime: 20 * 60 + 13
+        },
+
+        "heavy waves southern ocean": {
+            inTime: 20 * 60 + 24,
+            outTime: 23 * 60 + 4
+        },
+
+        "cape horn": {
+            inTime: 23 * 60 + 16,
+            outTime: 24 * 60 + 42
+        },
+
+        "sepia storm footage": {
+            inTime: 24 * 60 + 43,
+            outTime: 26 * 60 + 23
+        },
+
+        "sunset footage": {
+            inTime: 27 * 60 + 21,
+            outTime: 27 * 60 + 55
+        },
+
+        "rainbow": {
+            inTime: 28 * 60 + 25,
+            outTime: 28 * 60 + 47
+        },
+
+        "square waves": {
+            inTime: 29 * 60,
+            outTime: 29 * 60 + 14
+        },
+
+        "albatross again": {
+            inTime: 29 * 60 + 15,
+            outTime: 29 * 60 + 36
+        },
+
+        "flag mask": {
+            inTime: 30 * 60 + 33,
+            outTime: 30 * 60 + 42
+        }
     }
 });
 
@@ -36,6 +143,11 @@ fluid.defaults("colin.theLongWay.glRenderer", {
     },
 
     uniforms: {
+        modulationLayerSampler: {
+            type: "1i",
+            values: 1
+        },
+
         videoSize: {
             type: "2f",
             values: [
--- src/shaders/the-long-distributed-way.frag
+++ src/shaders/the-long-distributed-way.frag
@@ -1,16 +1,69 @@
 precision highp float;
 
 uniform sampler2D layerSampler;
+uniform sampler2D modulationLayerSampler;
 uniform vec2 textureSize;
 uniform vec2 videoSize;
 
-void main(void) {
-    if (gl_FragCoord.x > videoSize.x || gl_FragCoord.y > videoSize.y) {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    } else {
-        vec2 coords = vec2(gl_FragCoord.x / videoSize.x, gl_FragCoord.y / videoSize.y);
-        vec4 topFrag = texture2D(layerSampler, coords);
+// From https://github.com/AnalyticalGraphicsInc/cesium/blob/master/Source/Shaders/Builtin/Functions/luminance.glsl
+float luminance(vec3 rgb) {
+    const vec3 W = vec3(0.2125, 0.7154, 0.0721);
+    return dot(rgb, W);
+}
 
-        gl_FragColor = topFrag;
+vec4 black() {
+    return vec4(0.0, 0.0, 0.0, 1.0);
+}
+
+vec4 blockyLum(void) {
+    vec2 scaledCoords = gl_FragCoord.xy * (videoSize / textureSize);
+    vec2 vidCoords = scaledCoords.xy / videoSize.xy;
+    vec4 sampled = texture2D(layerSampler, scaledCoords.xy / videoSize.xy);
+    float lum = luminance(sampled.rgb);
+    vec3 smoothed = smoothstep(sampled.rgb, black().rgb, vec3(0.9, 0.9, 0.9));
+
+    return vec4(smoothed * 10.0, 1.0);
+}
+
+vec4 smoothLum(void) {
+    vec2 scaledCoords = gl_FragCoord.xy * (videoSize / textureSize);
+    vec2 remainder = fract(scaledCoords);
+
+    // TODO: Why these numbers? What am I doing?
+    if (remainder.x > 0.07 || remainder.y > 0.07) {
+        // We're "in between" pixels in the video and
+        // should just render a black fragment.
+        return black();
     }
+
+    vec2 vidCoords = scaledCoords.xy / videoSize.xy;
+    vec4 sampled = texture2D(layerSampler, scaledCoords.xy / videoSize.xy);
+    float lum = luminance(sampled.rgb);
+    vec3 smoothed = smoothstep(sampled.rgb, black().rgb, vec3(lum, lum, lum));
+
+    return vec4(smoothed, 1.0);
+}
+
+vec4 spacedLum(void) {
+    vec2 scaledCoords = gl_FragCoord.xy * (videoSize / textureSize);
+    vec2 remainder = fract(scaledCoords);
+
+    // TODO: Why these numbers? What am I doing?
+    if (remainder.x > 0.07 || remainder.y > 0.07) {
+        // We're "in between" pixels in the video and
+        // should just render a black fragment.
+        return black();
+    }
+
+    vec2 vidCoords = scaledCoords.xy / videoSize.xy;
+    vec4 sampled = texture2D(layerSampler, scaledCoords.xy / videoSize.xy);
+    float lum = luminance(sampled.rgb);
+
+    return lum < 0.9 ? black() : sampled;
+}
+
+void main(void) {
+    vec2 scaledCoords = gl_FragCoord.xy * (videoSize / textureSize);
+
+    gl_FragColor = /*spacedLum() +  */blockyLum()/* + (texture2D(loopLayerSampler, scaledCoords.xy / videoSize.xy) / 5.0)*/;
 }
